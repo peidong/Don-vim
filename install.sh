911ba388e13a25383f67cfe5cc32c474fd516b88
peidong <peidong@outlook.com>
2016-01-30 13:11:51 -0800

    install shell

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env sh
 cd ~
-
+# mv .vim .vim_old
+# mv .vimrc .vimrc_old
+mv pei-vim .vim
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
+# cp ~/.vim/vimrc.before.local ~/.vimrc.before.local
+mkdir ~/.undodir/
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+./.vim/bundle/neobundle.vim/bin/neoinstall
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
